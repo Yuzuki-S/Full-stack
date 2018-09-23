fc44d8e103a83056de116091c63f4dbff1b256ce
Yuzuki-S <yuzuki.sadoh@gmail.com>
2018-09-23 16:50:22 +1200

    deploy

--- knexfile.js
+++ knexfile.js
@@ -1,28 +1,29 @@
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './dev.sqlite3'
+      filename: "./dev.sqlite3"
     },
     useNullAsDefault: true
   },
 
   test: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: ':memory:'
-    },
-    seeds: {
-      directory: './tests/seeds'
+      filename: ":memory:"
     },
     useNullAsDefault: true
   },
 
   production: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: "knex_migrations"
     }
   }
-}
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,29 @@
-const env = process.env.NODE_ENV || 'development'
-module.exports = require(`./webpack.${env}.js`)
+const path = require('path')
+
+module.exports = {
+    entry: './client/index.js',
+    output: {
+      path: path.join(__dirname + '/public'),
+      filename: 'bundle.js'
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use:{
+              loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    devtool: 'source-map'
+  }
